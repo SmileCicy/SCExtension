2e26f3b020404e7182f20c743fced7367f23ce1b
SmileCicy <592751156@qq.com>
2023-05-05 15:41:15 +0800

    fix UICollectionView+

--- Sources/SCExtension/UICollectionView+.swift
+++ Sources/SCExtension/UICollectionView+.swift
@@ -11,15 +11,20 @@ import UIKit
 public extension NameSpaceWrapper where Base: UICollectionView {
     
     @discardableResult
-    func register<T: UICollectionViewCell>(_ className: T.Type, _ index: IndexPath) -> T {
-        guard let cell = self.base.dequeueReusableCell(withReuseIdentifier: String(describing: className), for: index) as? T else {
-            fatalError("did not register cell \(String(describing: className))")
+    func register<T: UICollectionViewCell>(_ name: T.Type) -> Base {
+        self.base.register(T.self, forCellWithReuseIdentifier: String(describing: name))
+        return self.base
+    }
+    
+    func dequeueCell<T: UICollectionViewCell>(_ name: T.Type, _ index: IndexPath) -> T {
+        guard let cell = self.base.dequeueReusableCell(withReuseIdentifier: String(describing: name), for: index) as? T else {
+            fatalError("did not register cell \(String(describing: name))")
         }
         return cell
     }
     
     @discardableResult
-    func registerHeader<T: UICollectionReusableView>(_ className: T.Type) -> UICollectionView {
+    func registerHeader<T: UICollectionReusableView>(_ className: T.Type) -> Base {
         self.base.register(T.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: String(describing: className))
         return self.base
     }
@@ -32,19 +37,19 @@ public extension NameSpaceWrapper where Base: UICollectionView {
     }
     
     @discardableResult
-    func delegate(_ delegate: UICollectionViewDelegate) -> UICollectionView {
+    func delegate(_ delegate: UICollectionViewDelegate) -> Base {
         self.base.delegate = delegate
         return self.base
     }
     
     @discardableResult
-    func datasource(_ datasource: UICollectionViewDataSource) -> UICollectionView {
+    func datasource(_ datasource: UICollectionViewDataSource) -> Base {
         self.base.dataSource = datasource
         return self.base
     }
     
     @discardableResult
-    func multSelect(_ allow: Bool = true) -> UICollectionView {
+    func multSelect(_ allow: Bool = true) -> Base {
         self.base.allowsMultipleSelection = allow
         return self.base
     }
